1b8bca5dc7031d7f21dae26bc216461d8dfc60e3
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-07-19 17:35:51 +0530

    Update botHandeling.py

--- plugins/components/botHandeling.py
+++ plugins/components/botHandeling.py
@@ -55,9 +55,9 @@ async def set_stream_link(client, message):
             await db.set_stream_link(link)
             await message.reply("Successfully set stream link!")
         else:
-            await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+            await message.reply("Usage: /stream https://t.me/SvFilmsX_Official or http://t.me/SvFilmsX_Official")
     except IndexError:
-        await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+        await message.reply("Usage: /stream https://t.me/SvFilmsX_Official or http://t.me/SvFilmsX_Official")
         return
 
 
@@ -88,17 +88,17 @@ def checkIfLinkIsValid(link):
 @Client.on_message(filters.command("m_grp") & filters.user(ADMINS))
 async def m_grp(client, message):
     links = []
-    link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is bisal_files")
+    link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is SvFilmsX")
     if link.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/SvFilmsX_Official")
     else:
         if checkIfLinkIsValid(link.text):
             links.append(link.text)
         else:
             await message.reply("Invalid link")
-    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is bisal_files")
+    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is SvFilmsX")
     if link1.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/SvFilmsX_Official")
     else:
         if checkIfLinkIsValid(link1.text):
             links.append(link1.text)
